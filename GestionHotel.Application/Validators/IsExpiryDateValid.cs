0f98bbd739a67b32c455a73fcefced5ace180700
0m8y <msoubi@icloud.com>
2025-06-18 00:23:46 +0200

    add validator

---
+++ GestionHotel.Application/Validators/IsExpiryDateValid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace GestionHotel.Application.Validators
+{
+    public static class CardValidator
+    {
+        public static bool IsExpiryDateValid(string expiryDate)
+        {
+            if (!DateTime.TryParseExact("01/" + expiryDate, "dd/MM/yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+                return false;
+
+            return date >= DateTime.Today;
+        }
+    }
+}
